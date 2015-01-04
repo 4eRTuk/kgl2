17f7ad427aa118da051a3f2443161ffaf47cdad3
Stanislav Petryakov <become.glory@yandex.ru>
2015-01-04 14:42:44 +0300

    Lastname typo, app's iml/gradle added

--- android/app/src/main/java/kg/fucking/shit/kgl2/Sofa.java
+++ android/app/src/main/java/kg/fucking/shit/kgl2/Sofa.java
@@ -1,4 +1,4 @@
-/*          Copyright © 2014 Stanislav Petiakov
+/*          Copyright © 2014 Stanislav Petriakov
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
